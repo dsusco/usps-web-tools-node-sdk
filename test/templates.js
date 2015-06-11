08f39213d079ab2f0862b9727cb8a3bcbe2bcfbe
David Susco <dsusco@gmail.com>
2015-06-11 14:21:43 -0400

    added handlebars templates

---
+++ test/templates.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var templates = require('../lib/templates');
+
+module.exports = {
+  templates: function(test) {
+    test.expect(1);
+
+    test.equal(Object.keys(templates).length, 1, 'number of templates not what was expected');
+
+    test.done();
+  }
+};
