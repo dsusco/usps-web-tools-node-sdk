f225440bd5e93b49d4c2a67328b16d7447e53885
David Susco <dsusco@gmail.com>
2015-06-16 14:04:03 -0400

    reformated test object

--- test/apis/trackAndConfirm.js
+++ test/apis/trackAndConfirm.js
@@ -16,7 +16,13 @@ module.exports = {
         test.ifError(error);
         test.deepEqual(
           response,
-          {"trackInfo":[{"trackSummary":"The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later." },{"trackSummary":"The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later." },{"error":{"number":"-2147219301","description":"Delivery status information is not available for your item via this web site.","helpFile":"","helpContext":""}}]},
+          { trackInfo:
+             [ { trackSummary: 'The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later.' },
+               { trackSummary: 'The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later.' },
+               { error: { number: '-2147219301',
+                          description: 'Delivery status information is not available for your item via this web site.',
+                          helpFile: '',
+                          helpContext: '' } } ] },
           'trackAndConfirm.track does not return expected response'
         );
 
@@ -35,7 +41,12 @@ module.exports = {
         test.ifError(error);
         test.deepEqual(
           response,
-          {"trackInfo":{"error":{"number":"-2147219302","description":"The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later.","helpFile":"","helpContext":""}}},
+          { trackInfo:
+             { error:
+                { number: '-2147219302',
+                  description: 'The Postal Service could not locate the tracking information for your request. Please verify your tracking number and try again later.',
+                  helpFile: '',
+                  helpContext: '' } } },
           'trackAndConfirm.trackField does not return expected response'
         );
 
