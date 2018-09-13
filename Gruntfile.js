d487a28572abb83bc4307e4f202870250528b6ff
David Susco <dsusco@gmail.com>
2018-09-13 12:13:19 -0400

    added bump task

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,7 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
